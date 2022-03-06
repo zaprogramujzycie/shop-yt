230a06519e411816df466166b56e2a24a0f24b71
zaprogramujzycie <zaprogramujzycie@gmail.com>
2022-03-06 12:04:10 +0100

    #44 Relacja ManyToMany na przykladzie funkcjonalnosci zamowien

---
+++ database/migrations/2021_12_19_185215_create_order_product_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->foreignId('order_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
